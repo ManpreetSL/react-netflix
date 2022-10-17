3d33b0a46410158886996ff8b628b27a223678e2
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-10-17 22:21:07 +0100

    Added skeleton for jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Jumbotron from './components/jumbotron';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Netflix</h1>
+        <Jumbotron />
       </header>
     </div>
   );
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Inner from './styles/jumbotron';
+
+function Jumbotron() {
+  return (
+    <Inner>
+      <div>Jumbotron</div>
+    </Inner>
+  );
+}
+
+export default Jumbotron;
