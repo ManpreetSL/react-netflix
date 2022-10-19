c58f6ac424f6e16d00d425ad124b8cc56121edd6
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-10-19 21:58:13 +0100

    Set up styled components to lay out the jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Jumbotron from './components/jumbotron';
+import jumboFixture from './fixtures/jumbo.json';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Netflix</h1>
-        <Jumbotron />
       </header>
+      <main>
+        <Jumbotron.Container>
+          {jumboFixture.map((item) => (
+            <Jumbotron key={item.id} direction={item.direction}>
+              <Jumbotron.Pane>
+                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+              </Jumbotron.Pane>
+              <Jumbotron.Pane>
+                <Jumbotron.Image src={item.image} alt={item.alt} />
+              </Jumbotron.Pane>
+            </Jumbotron>
+          ))}
+        </Jumbotron.Container>
+      </main>
     </div>
   );
 }
